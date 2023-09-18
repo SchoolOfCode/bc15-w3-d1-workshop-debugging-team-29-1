e9e01a59126f9119d3937725a00b79ebb1867ffb
Mahad Mohamed <mahad.mohamed1@outlook.com>
2023-09-18 16:55:57 +0100

    ticket 2 completed

--- ticket-2/index.js
+++ ticket-2/index.js
@@ -1,11 +1,16 @@
-let initials = "";
-names = ["Abe Maxwell", "Martin Boxer Harris", "Felicia Rita Sarah Jones", "Mary Petunia"];
 function extractInitialsFromFullName(fullName) {
-let names = fullName.split(" ");
-for (let i = 0; i < names.length; i++) {
-    let currentName = names[i];
-    let currentInitial = currentName.at(0);
+    /** Assumes that each name within the full name is separated by a "space" that can be used as a separator/delimiter.  */
+    const         names            = fullName.split(" ");
+    let initials = "";
+
+
+    for (let i = 0; i < names.length; i++) {
+    const currentName = names[i];
+    const currentInitial = currentName.at(0);
     initials += currentInitial;
     }
+    return initials;
 }
-extractInitialsFromFullName("Abe Maxwell")
+
+console.log(extractInitialsFromFullName('Abi Maxwell'))
+
